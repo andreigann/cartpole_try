090cc8cd4fed35c4bf6446121f2cd8772c692e13
Andrei Gherghel <andrei.gherghel@hotmail.com>
2019-09-22 22:06:01 +0300

    extracted dynamic parameters

--- cartpole.py
+++ cartpole.py
@@ -12,17 +12,32 @@ from scores.score_logger import ScoreLogger
 
 ENV_NAME = "CartPole-v1"
 
-GAMMA = 0.95
-LEARNING_RATE = 0.001 # unused as we use Experience Replay type of Q-Learning
+GAMMA = 0.80
+LEARNING_RATE = 0.001  # unused as we use Experience Replay type of Q-Learning
 # See more on Experience Replay here: https://datascience.stackexchange.com/questions/20535/what-is-experience-replay-and-what-are-its-benefits
 
-MEMORY_SIZE = 1000 # used only for Non-Incremental learning, i.e. partial_fit=False
-BATCH_SIZE = 300
+MEMORY_SIZE = 5000  # used only for Non-Incremental learning, i.e. partial_fit=False
+BATCH_SIZE = 500
 
 EXPLORATION_MAX = 1.0
-EXPLORATION_MIN = 0.05
-EXPLORATION_DECAY = 0.80
+EXPLORATION_MIN = 0.01
+EXPLORATION_DECAY = 0.996
 
+IS_PARTIAL_FIT = True
+RENDER = False
+SCORE_LOGGER = False
+MY_LOGS = True
+NO_ITERATIONS = 100
+
+DEFAULT_EPSILON = 0.1
+
+def get_regressor():
+    if IS_PARTIAL_FIT:
+        return SGDRegressor()
+
+    return AdaBoostRegressor(n_estimators=50)
+    # return RandomForestRegressor(max_depth=2, random_state=0, n_estimators=100)
+    # return LGBMRegressor(n_estimators=100, n_jobs=-1);
 
 class DQNSolver:
 
@@ -33,20 +48,10 @@ class DQNSolver:
         self.memory = deque(maxlen=MEMORY_SIZE)
         self._is_partial_fit = is_partial_fit
 
-        if is_partial_fit:
-            # Here you can use only Incremental Models: https://scikit-learn.org/0.18/modules/scaling_strategies.html
-            regressor = SGDRegressor()
-            self.model = MultiOutputRegressor(regressor)
-        else:
-            # Here you can use whatever regression model you want, simple or Incremental
-            # The sklearn regression models can be found by searching for "regress" at https://scikit-learn.org/stable/modules/classes.html
+        self.predict_mode = False
 
-            # Ex:
-            #regressor = RandomForestRegressor(max_depth=2, random_state=0, n_estimators=100)
-            #regressor = LGBMRegressor(n_estimators=100, n_jobs=-1)
-
-            regressor = AdaBoostRegressor(n_estimators=10)
-            self.model = MultiOutputRegressor(regressor)
+        regressor = get_regressor()
+        self.model = MultiOutputRegressor(regressor)
 
         self.isFit = False
 
@@ -55,7 +60,17 @@ class DQNSolver:
 
     def act(self, state):
         if np.random.rand() < self.exploration_rate:
+            # print("Random guess")
+            # if self.predict_mode:
+            #     self.predict_mode = False;
+            #     print("Switched to random mode")
             return random.randrange(self.action_space)
+
+        # print("Predict mode")
+        # if not self.predict_mode:
+        #     self.predict_mode = True
+        #     print("Switched to predict mode")
+
         if self.isFit == True:
             q_values = self.model.predict(state)
         else:
@@ -71,14 +86,15 @@ class DQNSolver:
             # and the rest of the history is stored within the model, indirectly through learning
             self.memory = deque(maxlen=BATCH_SIZE)
         else:
-            batch = random.sample(self.memory, int(len(self.memory)/1))
+            batch = random.sample(self.memory, int(len(self.memory) / 1))
         X = []
         targets = []
         for state, action, reward, state_next, terminal in batch:
             q_update = reward
             if not terminal:
                 if self.isFit:
-                    q_update = (reward + GAMMA * np.amax(self.model.predict(state_next)[0]))   #Return the maximum of an array or maximum along an axis
+                    q_update = (reward + GAMMA * np.amax(
+                        self.model.predict(state_next)[0]))  # Return the maximum of an array or maximum along an axis
                 else:
                     q_update = reward
             if self.isFit:
@@ -86,7 +102,7 @@ class DQNSolver:
             else:
                 q_values = np.zeros(self.action_space).reshape(1, -1)
             q_values[0][action] = q_update
-            
+
             X.append(list(state[0]))
             targets.append(q_values[0])
 
@@ -105,9 +121,10 @@ def cartpole():
     score_logger = ScoreLogger(ENV_NAME)
     observation_space = env.observation_space.shape[0]
     action_space = env.action_space.n
-    dqn_solver = DQNSolver(action_space, is_partial_fit=True)
+    dqn_solver = DQNSolver(action_space, is_partial_fit=IS_PARTIAL_FIT)
     run = 0
-    while True:
+    current_max = 0
+    while run < NO_ITERATIONS:
         run += 1
         state = env.reset()
         state = np.reshape(state, [1, observation_space])
@@ -115,17 +132,27 @@ def cartpole():
         # run = 0
         while True:
             step += 1
-            env.render()
+            if RENDER:
+                env.render()
             action = dqn_solver.act(state)
             state_next, reward, terminal, info = env.step(action)
+
+            if MY_LOGS:
+                print(state_next, reward, terminal)
+
             reward = reward if not terminal else -reward
 
-            state_next = np.reshape(state_next, [1, observation_space])  # Gives a new shape to an array without changing its data.
+            state_next = np.reshape(state_next,
+                                    [1, observation_space])  # Gives a new shape to an array without changing its data.
             dqn_solver.remember(state, action, reward, state_next, terminal)
             state = state_next
+            current_max = max(current_max, step)
             if terminal:
-                print("Run: " + str(run) + ", exploration_rate: " + str(dqn_solver.exploration_rate) + ", episodes: " + str(step))
-                # score_logger.add_score(step, run)
+                print("Run: " + str(run) + ", exploration_rate: " + str(
+                    dqn_solver.exploration_rate) + ", episodes: " + str(step), ", max episodes reached: ",
+                      str(current_max))
+                if (SCORE_LOGGER):
+                    score_logger.add_score(step, run)
                 break
             dqn_solver.experience_replay()
 
